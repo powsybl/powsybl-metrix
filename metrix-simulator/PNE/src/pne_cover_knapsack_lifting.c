d63614ff6e4768b0505ad2a6241f548309f48e78
Le Courtois Florent <florent.lecourtois@rte-france.com>
2021-03-08 08:48:47 +0100

    Add license to source files
    Signed-off-by: Le Courtois Florent <florent.lecourtois@rte-france.com>

--- metrix-simulator/PNE/src/pne_cover_knapsack_lifting.c
+++ metrix-simulator/PNE/src/pne_cover_knapsack_lifting.c
@@ -1,3 +1,13 @@
+//
+// Copyright (c) 2021, RTE (http://www.rte-france.com)
+// See AUTHORS.txt
+// All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
+//
+
 /***********************************************************************
 
    FONCTION: Lifting d'une couverture de sac a dos par la methode
