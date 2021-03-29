90bb39497ebb0ff796fed708ce3c9ebcb0d1180a
Le Courtois Florent <florent.lecourtois@rte-france.com>
2021-03-29 14:48:55 +0200

    Update CI and readme
    Signed-off-by: Le Courtois Florent <florent.lecourtois@rte-france.com>

--- install.sh
+++ install.sh
@@ -18,39 +18,20 @@ if [ -z "$INSTALL_DIR" ] ; then
 fi
 echo $INSTALL_DIR
 
-echo "Choose install type (full / metrix) :"
-read INSTALL_TYPE
-if [ -z "$INSTALL_TYPE" ] ; then
-  INSTALL_TYPE="full"
-fi
-if [ $INSTALL_TYPE != "full" -a $INSTALL_TYPE != "metrix" ] ; then
-  echo "Allowed value for install type is metrix or full"
-  exit 1
-fi
-echo $INSTALL_TYPE
-exit 0
-
 echo "Preparing install directory"
 mkdir -p $INSTALL_DIR
 
 echo "Installing metrix"
 pushd $CURDIR/metrix-simulator
 mkdir -p build
-cd build
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR .
+mkdir -p build/external
+cd build/external
+cmake ../../external
+cmake --build .
+cd ..
+cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR ..
 cmake --build . --target install
 popd
-
-if [ "$INSTALL_TYPE" == "full" ] ; then
-  echo "Installing powsybl-metrix"
-  pushd $CURDIR
-  mvn clean package
-  cp -r distribution/target/metrix/bin/* $INSTALL_DIR/bin
-  cp -r distribution/target/metrix/etc/* $INSTALL_DIR/etc
-  cp -r distribution/target/metrix/lib $INSTALL_DIR/lib
-  cp -r distribution/target/metrix/share $INSTALL_DIR/share
-  popd
-fi
 
 echo "Add to PATH ? (y/n)"
 read ADD_TO_PATH
