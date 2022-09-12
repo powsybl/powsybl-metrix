560066bae373a39e0c64aeeea920e48895a2510e
Ulysse Prieto <uprieto@localsolver.com>
2022-09-12 12:06:30 +0200

    Disable some xpress tests

---
+++ metrix-simulator/tests/CTestCustom.cmake
@@ -0,0 +1,36 @@
+set(CTEST_CUSTOM_TESTS_IGNORE
+    xpress_probleme_bornes_ecart_1
+    xpress_probleme_bornes_ecart_2
+    xpress_contrainte_sans_action_possible_opf
+    xpress_contraintes_sans_action_possible
+    xpress_tres_long
+    xpress_tout_fixe
+    xpress_variations_marginales_sur_parades
+    xpress_temps_calcul_pne
+    xpress_bug_pne
+    xpress_resultats_surcharges_uniquement
+    xpress_connexite_base_variant
+    xpress_noeud_connexe_isole
+    xpress_incidents_piquage
+    xpress_pb_inc_rompant_connex
+    xpress_incidents_rompant_connexite_avec_parades
+    xpress_incident_ne_rompt_plus_connexite
+    xpress_inc_connexite_et_quad_deconnecte
+    xpress_parade_cree_2_noeuds_isoles
+    xpress_isolated_node_after_base_case_and_variant
+    xpress_contraintes_equivalentes
+    xpress_parades
+    xpress_parades_et_TD
+    xpress_limitation_nb_actions
+    xpress_contrainte_cree_par_parade
+    xpress_parades_pas_vraiment_equivalentes
+    xpress_parade_inverse_transit
+    xpress_delestage_charges_negatives
+    xpress_curatif_sans_preventif
+    xpress_hvdcWatchSection
+    xpress_mapping_prises_td
+    xpress_redispatching
+    xpress_pompage_hr_necessaire
+    xpress_hvdc_en_emulation_AC
+    xpress_variantes_regroupees_complexe
+)
