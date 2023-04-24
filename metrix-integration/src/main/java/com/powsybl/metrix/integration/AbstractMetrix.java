c552c34ba2bff381e7b5eb470a87c5ebaa495f04
marifunf <marianne.funfrock@rte-france.com>
2023-04-24 17:05:36 +0200

    [IMG-1686] Metrix post processing with ITAM thresholds
    Signed-off-by: marifunf <marianne.funfrock@rte-france.com>

--- metrix-integration/src/main/java/com/powsybl/metrix/integration/AbstractMetrix.java
+++ metrix-integration/src/main/java/com/powsybl/metrix/integration/AbstractMetrix.java
@@ -21,7 +21,6 @@ import com.powsybl.metrix.integration.metrix.MetrixAnalysisResult;
 import com.powsybl.metrix.mapping.*;
 import com.powsybl.timeseries.ReadOnlyTimeSeriesStore;
 import com.powsybl.timeseries.TimeSeriesIndex;
-import com.powsybl.timeseries.ast.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,13 +29,13 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+
+import static com.powsybl.metrix.integration.MetrixPostProcessingTimeSeries.getPostProcessingTimeSeries;
 
 public abstract class AbstractMetrix {
 
@@ -51,11 +50,6 @@ public abstract class AbstractMetrix {
     protected static final String LOG_FILE_DETAIL_PREFIX = "metrix";
 
     public static final String MAX_THREAT_PREFIX = MetrixOutputData.MAX_THREAT_NAME + "1_FLOW_";
-    public static final String BASECASE_LOAD_PREFIX = "basecaseLoad_";
-    public static final String BASECASE_OVERLOAD_PREFIX = "basecaseOverload_";
-    public static final String OUTAGE_LOAD_PREFIX = "outageLoad_";
-    public static final String OUTAGE_OVERLOAD_PREFIX = "outageOverload_";
-    public static final String OVERALL_OVERLOAD_PREFIX = "overallOverload_";
 
     protected final Reader remedialActionsReader;
 
@@ -271,159 +265,6 @@ public abstract class AbstractMetrix {
         while (Files.exists(path = workingDir.resolve(String.format(logDetailFileNameFormat, i)))) {
             addLogToArchive(path, logArchive);
             i++;
-        }
-    }
-
-    // Post-processing methods
-    public static Map<String, NodeCalc> getPostProcessingTimeSeries(MetrixDslData dslData,
-                                                             TimeSeriesMappingConfig mappingConfig,
-                                                             ReadOnlyTimeSeriesStore store,
-                                                             String nullableSchemaName) {
-        Map<String, NodeCalc> postProcessingTimeSeries = new HashMap<>();
-        if (dslData != null && mappingConfig != null) {
-            Map<String, NodeCalc> calculatedTimeSeries = new HashMap<>(mappingConfig.getTimeSeriesNodes());
-            createBasecasePostprocessingTimeSeries(dslData, mappingConfig, postProcessingTimeSeries, calculatedTimeSeries, store, nullableSchemaName);
-            createOutagePostprocessingTimeSeries(dslData, mappingConfig, postProcessingTimeSeries, calculatedTimeSeries, store, nullableSchemaName);
-        }
-        return postProcessingTimeSeries;
-    }
-
-    private static NodeCalc createLoadTimeSeries(NodeCalc flowTimeSeries, NodeCalc ratingTimeSeries) {
-        return BinaryOperation.multiply(BinaryOperation.div(flowTimeSeries, ratingTimeSeries), new FloatNodeCalc(100));
-    }
-
-    private static NodeCalc createLoadTimeSeries(NodeCalc flowTimeSeries, NodeCalc ratingTimeSeriesOrEx, NodeCalc ratingTimeSeriesExOr) {
-        if (ratingTimeSeriesOrEx == ratingTimeSeriesExOr) {
-            return createLoadTimeSeries(flowTimeSeries, ratingTimeSeriesOrEx);
-        } else {
-            NodeCalc zero = new IntegerNodeCalc(0);
-            NodeCalc ratingTimeSeries = BinaryOperation.plus(BinaryOperation.multiply(BinaryOperation.greaterThan(flowTimeSeries, zero), ratingTimeSeriesOrEx),
-                    BinaryOperation.multiply(BinaryOperation.lessThan(flowTimeSeries, zero), ratingTimeSeriesExOr));
-            return createLoadTimeSeries(flowTimeSeries, ratingTimeSeries);
-        }
-    }
-
-    private static NodeCalc createOverloadTimeSeries(NodeCalc flowTimeSeries, NodeCalc ratingTimeSeriesOrEx, NodeCalc ratingTimeSeriesExOr) {
-        NodeCalc positiveOverloadTimeSeries = BinaryOperation.minus(flowTimeSeries, ratingTimeSeriesOrEx);
-        NodeCalc negativeRatingTimeSeries = UnaryOperation.negative(ratingTimeSeriesExOr);
-        NodeCalc negativeOverloadTimeSeries = BinaryOperation.minus(flowTimeSeries, negativeRatingTimeSeries);
-        return BinaryOperation.plus(BinaryOperation.multiply(BinaryOperation.greaterThan(flowTimeSeries, ratingTimeSeriesOrEx), positiveOverloadTimeSeries),
-                BinaryOperation.multiply(BinaryOperation.lessThan(flowTimeSeries, negativeRatingTimeSeries), negativeOverloadTimeSeries));
-    }
-
-    private static NodeCalc createOverallOverloadTimeSeries(NodeCalc basecaseOverloadTimeSeries, NodeCalc outageOverloadTimeSeries) {
-        return BinaryOperation.plus(UnaryOperation.abs(basecaseOverloadTimeSeries), UnaryOperation.abs(outageOverloadTimeSeries));
-    }
-
-    private static void createBasecasePostprocessingTimeSeries(MetrixDslData metrixDslData,
-                                                               TimeSeriesMappingConfig mappingConfig,
-                                                               Map<String, NodeCalc> postProcessingTimeSeries,
-                                                               Map<String, NodeCalc> calculatedTimeSeries,
-                                                               ReadOnlyTimeSeriesStore store,
-                                                               String nullableSchemaName) {
-
-        for (String branch : metrixDslData.getBranchMonitoringNList()) {
-            MetrixVariable threshold = metrixDslData.getBranchMonitoringStatisticsThresholdN(branch);
-            MetrixVariable thresholdEndOr = threshold == MetrixVariable.thresholdN ? MetrixVariable.thresholdNEndOr : MetrixVariable.analysisThresholdNEndOr;
-            if (mappingConfig.getTimeSeriesName(new MappingKey(threshold, branch)) != null) {
-                createBasecasePostprocessingTimeSeries(branch, threshold, thresholdEndOr, mappingConfig, postProcessingTimeSeries, calculatedTimeSeries, store, nullableSchemaName);
-            }
-        }
-    }
-
-    private static void createBasecasePostprocessingTimeSeries(String branch,
-                                                               MetrixVariable thresholdN,
-                                                               MetrixVariable thresholdNEndOr,
-                                                               TimeSeriesMappingConfig mappingConfig,
-                                                               Map<String, NodeCalc> postProcessingTimeSeries,
-                                                               Map<String, NodeCalc> calculatedTimeSeries,
-                                                               ReadOnlyTimeSeriesStore store,
-                                                               String nullableSchemaName) {
-        try {
-            if (!store.timeSeriesExists(MetrixOutputData.FLOW_NAME + branch)) {
-                LOGGER.debug("FLOW time-series not found for {}", branch);
-                return;
-            }
-            LOGGER.debug("Creating basecase postprocessing time-series for {}", branch);
-            NodeCalc flowTimeSeries = new TimeSeriesNameNodeCalc(MetrixDataName.getNameWithSchema(MetrixOutputData.FLOW_NAME + branch, nullableSchemaName));
-            String ratingTimeSeriesName = mappingConfig.getTimeSeriesName(new MappingKey(thresholdN, branch));
-            NodeCalc ratingTimeSeriesOrEx = calculatedTimeSeries.computeIfAbsent(ratingTimeSeriesName, TimeSeriesNameNodeCalc::new);
-
-            NodeCalc ratingTimeSeriesExOr = ratingTimeSeriesOrEx;
-            if (thresholdNEndOr != null) {
-                ratingTimeSeriesName = mappingConfig.getTimeSeriesName(new MappingKey(thresholdNEndOr, branch));
-                if (ratingTimeSeriesName != null) {
-                    ratingTimeSeriesExOr = calculatedTimeSeries.computeIfAbsent(ratingTimeSeriesName, TimeSeriesNameNodeCalc::new);
-                }
-            }
-
-            // Basecase load
-            postProcessingTimeSeries.put(MetrixDataName.getNameWithSchema(BASECASE_LOAD_PREFIX + branch, nullableSchemaName), createLoadTimeSeries(flowTimeSeries, ratingTimeSeriesOrEx, ratingTimeSeriesExOr));
-            // Basecase overload
-            postProcessingTimeSeries.put(MetrixDataName.getNameWithSchema(BASECASE_OVERLOAD_PREFIX + branch, nullableSchemaName), createOverloadTimeSeries(flowTimeSeries, ratingTimeSeriesOrEx, ratingTimeSeriesExOr));
-        } catch (IllegalStateException ise) {
-            LOGGER.debug("Monitored branch {} not found in network", branch);
-        }
-
-    }
-
-    private static void createOutagePostprocessingTimeSeries(MetrixDslData metrixDslData,
-                                                             TimeSeriesMappingConfig mappingConfig,
-                                                             Map<String, NodeCalc> postProcessingTimeSeries,
-                                                             Map<String, NodeCalc> calculatedTimeSeries,
-                                                             ReadOnlyTimeSeriesStore store,
-                                                             String nullableSchemaName) {
-
-        for (String branch : metrixDslData.getBranchMonitoringNkList()) {
-            MetrixVariable threshold = metrixDslData.getBranchMonitoringStatisticsThresholdNk(branch);
-            MetrixVariable thresholdEndOr = threshold == MetrixVariable.thresholdN1 ? MetrixVariable.thresholdN1EndOr : MetrixVariable.analysisThresholdNkEndOr;
-            if (mappingConfig.getTimeSeriesName(new MappingKey(threshold, branch)) != null) {
-                createOutagePostprocessingTimeSeries(branch, threshold, thresholdEndOr, mappingConfig, postProcessingTimeSeries, calculatedTimeSeries, store, nullableSchemaName);
-            }
-        }
-    }
-
-    private static void createOutagePostprocessingTimeSeries(String branch,
-                                                             MetrixVariable thresholdN1,
-                                                             MetrixVariable thresholdN1EndOr,
-                                                             TimeSeriesMappingConfig mappingConfig,
-                                                             Map<String, NodeCalc> postProcessingTimeSeries,
-                                                             Map<String, NodeCalc> calculatedTimeSeries,
-                                                             ReadOnlyTimeSeriesStore store,
-                                                             String nullableSchemaName) {
-        try {
-
-            if (!store.timeSeriesExists(MAX_THREAT_PREFIX + branch)) {
-                LOGGER.debug("MAX_THREAT time-series not found for {}", branch);
-                return;
-            }
-
-            LOGGER.debug("Creating outage postprocessing time-series for {}", branch);
-
-            NodeCalc flowTimeSeries = new TimeSeriesNameNodeCalc(MetrixDataName.getNameWithSchema(MAX_THREAT_PREFIX + branch, nullableSchemaName));
-            String ratingTimeSeriesName = mappingConfig.getTimeSeriesName(new MappingKey(thresholdN1, branch));
-            NodeCalc ratingTimeSeriesOrEx = calculatedTimeSeries.computeIfAbsent(ratingTimeSeriesName, TimeSeriesNameNodeCalc::new);
-
-            NodeCalc ratingTimeSeriesExOr = ratingTimeSeriesOrEx;
-            if (thresholdN1EndOr != null) {
-                ratingTimeSeriesName = mappingConfig.getTimeSeriesName(new MappingKey(thresholdN1EndOr, branch));
-                if (ratingTimeSeriesName != null) {
-                    ratingTimeSeriesExOr = calculatedTimeSeries.computeIfAbsent(ratingTimeSeriesName, TimeSeriesNameNodeCalc::new);
-
-                }
-            }
-
-            // Outage load
-            postProcessingTimeSeries.put(MetrixDataName.getNameWithSchema(OUTAGE_LOAD_PREFIX + branch, nullableSchemaName), createLoadTimeSeries(flowTimeSeries, ratingTimeSeriesOrEx, ratingTimeSeriesExOr));
-            // Outage overload
-            NodeCalc outageOverLoadTimeSeries = createOverloadTimeSeries(flowTimeSeries, ratingTimeSeriesOrEx, ratingTimeSeriesExOr);
-            postProcessingTimeSeries.put(MetrixDataName.getNameWithSchema(OUTAGE_OVERLOAD_PREFIX + branch, nullableSchemaName), outageOverLoadTimeSeries);
-            NodeCalc basecaseOverLoadTimeSeries = postProcessingTimeSeries.get(BASECASE_OVERLOAD_PREFIX + branch);
-            if (!Objects.isNull(basecaseOverLoadTimeSeries)) {
-                postProcessingTimeSeries.put(MetrixDataName.getNameWithSchema(OVERALL_OVERLOAD_PREFIX + branch, nullableSchemaName), createOverallOverloadTimeSeries(basecaseOverLoadTimeSeries, outageOverLoadTimeSeries));
-            }
-        } catch (IllegalStateException ise) {
-            LOGGER.debug("Monitored branch {} not found in network", branch);
         }
     }
 }
