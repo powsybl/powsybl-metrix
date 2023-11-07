78d48da950503a5c3a169458d4ab39a22de7d59d
marifunf <marianne.funfrock@rte-france.com>
2023-11-07 14:19:22 +0100

    [IMG-2137] Test coverage improvement
    Signed-off-by: marifunf <marianne.funfrock@rte-france.com>

--- metrix-mapping/src/main/groovy/com/powsybl/metrix/mapping/TimeSeriesDslLoader.groovy
+++ metrix-mapping/src/main/groovy/com/powsybl/metrix/mapping/TimeSeriesDslLoader.groovy
@@ -21,7 +21,6 @@ import org.codehaus.groovy.control.customizers.ImportCustomizer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
 
