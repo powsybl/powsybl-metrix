6b780f6e6bc07a48b6187461070befc86b8388ef
marifunf <marianne.funfrock@rte-france.com>
2024-01-12 16:05:17 +0100

    add Remedial test
    Signed-off-by: marifunf <marianne.funfrock@rte-france.com>

---
+++ metrix-integration/src/test/java/com/powsybl/metrix/integration/remedials/RemedialTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+package com.powsybl.metrix.integration.remedials;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RemedialTest {
+
+    @Test
+    public void remedialTest() {
+        Remedial remedial = new Remedial(1, "cty", List.of("constraint"), List.of("branchToOpen"), List.of("branchToClose"), "actions");
+        assertEquals("cty", remedial.getContingency());
+        assertEquals(List.of("constraint"), remedial.getConstraint());
+        assertEquals(List.of("branchToOpen"), remedial.getBranchToOpen());
+        assertEquals(List.of("branchToClose"), remedial.getBranchToClose());
+        assertEquals("actions", remedial.getActions());
+    }
+
+    @Test
+    public void remedialNameFromActionsTest() {
+        Remedial remedial = new Remedial(1, "", Collections.emptyList(), List.of("branchToOpen"), List.of("branchToClose"), "");
+        assertEquals("branchToOpen;+branchToClose", remedial.getNameFromActions());
+    }
+}
