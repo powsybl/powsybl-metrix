7793e263cae5ce672fff05bb92e745e6ac8de53c
marifunf <marianne.funfrock@rte-france.com>
2024-02-08 09:39:50 +0100

    Update MetrixChunk.java
    Signed-off-by: marifunf <marianne.funfrock@rte-france.com>

--- metrix-integration/src/main/java/com/powsybl/metrix/integration/MetrixChunk.java
+++ metrix-integration/src/main/java/com/powsybl/metrix/integration/MetrixChunk.java
@@ -25,6 +25,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
