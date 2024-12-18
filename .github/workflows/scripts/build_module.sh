0e43edea7ee95efeda6580308083b027886209f2
Nicolas Rol <nicolas.rol@rte-france.com>
2024-12-18 11:59:56 +0100

    test slack notification
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- .github/workflows/scripts/build_module.sh
+++ .github/workflows/scripts/build_module.sh
@@ -8,9 +8,9 @@ $COMMAND
 BUILD_EXIT=$?
 
 if [ $BUILD_EXIT -ne 0 ]; then
-    echo "❌ $MODULE_NAME build FAILED" >> $BUILD_STATUS
+    echo "❌ $MODULE_NAME build FAILED" >> "$BUILD_STATUS"
 else
-    echo "✅ $MODULE_NAME build SUCCESS" >> $BUILD_STATUS
+    echo "✅ $MODULE_NAME build SUCCESS" >> "$BUILD_STATUS"
 fi
 
 exit $BUILD_EXIT
