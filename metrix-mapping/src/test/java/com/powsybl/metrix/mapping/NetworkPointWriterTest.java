acdc2a28d196416101cc2bcd534077a559974c14
Nicolas Rol <nicolas.rol@rte-france.com>
2025-07-03 11:42:15 +0200

    add tests
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- metrix-mapping/src/test/java/com/powsybl/metrix/mapping/NetworkPointWriterTest.java
+++ metrix-mapping/src/test/java/com/powsybl/metrix/mapping/NetworkPointWriterTest.java
@@ -36,6 +36,8 @@ import java.util.Objects;
 import java.util.TreeSet;
 
 import static com.powsybl.commons.test.ComparisonUtils.assertXmlEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * @author Paul Bui-Quang {@literal <paul.buiquang at rte-france.com>}
@@ -46,11 +48,20 @@ class NetworkPointWriterTest {
     private Network network;
 
     private final MappingParameters mappingParameters = MappingParameters.load();
+    private final TimeSeriesIndex index = RegularTimeSeriesIndex.create(Interval.parse("2015-01-01T01:00:00Z/2015-01-01T02:00:00Z"), Duration.ofHours(1));
+    private final String expectedDirectoryNameBase = "/expected/NetworkPointWriter/";
+    private List<TimeSeriesMapperObserver> networkPointWriterList;
 
     @BeforeEach
     void setUp() {
         this.fileSystem = Jimfs.newFileSystem(Configuration.unix());
         network = NetworkSerDe.read(Objects.requireNonNull(getClass().getResourceAsStream("/simpleNetwork.xml")));
+        Path networkOutputDir = fileSystem.getPath(".");
+
+        // Create NetworkPointWriter
+        DataSource dataSource = DataSourceUtil.createDataSource(networkOutputDir.resolve(network.getId()), null);
+        NetworkPointWriter networkPointWriter = new NetworkPointWriter(network, dataSource);
+        networkPointWriterList = List.of(networkPointWriter);
     }
 
     @AfterEach
@@ -59,11 +70,57 @@ class NetworkPointWriterTest {
     }
 
     @Test
+    void regulationModeTest() throws Exception {
+        // Resource directory
+        String expectedDirectoryName = expectedDirectoryNameBase + "regulationMode/";
+
+        // Mapping script
+        String script = String.join(System.lineSeparator(),
+            "mapToPhaseTapChangers {",
+            "    timeSeriesName 'regulation_mode_ts'",
+            "    filter {",
+            "        twoWindingsTransformer.id==\"FP.AND1  FTDPRA1  1\"",
+            "    }",
+            "    variable regulationMode",
+            "}");
+
+        // Timeseries
+        ReadOnlyTimeSeriesStore store = new ReadOnlyTimeSeriesStoreCache(
+            TimeSeries.createDouble("regulation_mode_ts", index, 0d, 2d)
+        );
+
+        assertMapping(script, store, expectedDirectoryName);
+    }
+
+    @Test
+    void regulationModeExceptionTest() {
+        // Mapping script
+        String script = String.join(System.lineSeparator(),
+            "mapToPhaseTapChangers {",
+            "    timeSeriesName 'regulation_mode_ts'",
+            "    filter {",
+            "        twoWindingsTransformer.id==\"FP.AND1  FTDPRA1  1\"",
+            "    }",
+            "    variable regulationMode",
+            "}");
+
+        // Timeseries
+        ReadOnlyTimeSeriesStore store = new ReadOnlyTimeSeriesStoreCache(
+            TimeSeries.createDouble("regulation_mode_ts", index, 3d, 3d)
+        );
+
+        // Create Mapper
+        TimeSeriesMapper mapper = prepareMapper(script, store);
+
+        // Launch mapper should throw an exception
+        AssertionError exception = assertThrows(AssertionError.class, () -> mapper.mapToNetwork(store, networkPointWriterList));
+        assertEquals("Unsupported regulation mode 3.0", exception.getMessage());
+    }
+
+    @Test
     void networkPointConstantVariantTest() throws Exception {
-
-        Path networkOutputDir = fileSystem.getPath(".");
-
-        String expectedDirectoryName = "/expected/NetworkPointWriter/";
+        // Resource directory
+        String expectedDirectoryName = expectedDirectoryNameBase + "full/";
 
         // Mapping script
         String script = String.join(System.lineSeparator(),
@@ -303,9 +360,6 @@ class NetworkPointWriterTest {
                 "    variable disconnected",
                 "}");
 
-        // Create time series space mock
-        TimeSeriesIndex index = RegularTimeSeriesIndex.create(Interval.parse("2015-01-01T01:00:00Z/2015-01-01T02:00:00Z"), Duration.ofHours(1));
-
         ReadOnlyTimeSeriesStore store = new ReadOnlyTimeSeriesStoreCache(
                 TimeSeries.createDouble("constant_ts1", index, 100d, 100d),
                 TimeSeries.createDouble("constant_ts2", index, 3000d, 3000d),
@@ -317,21 +371,27 @@ class NetworkPointWriterTest {
                 TimeSeries.createDouble("regulation_mode_ts", index, 0d, 1d)
         );
 
+        assertMapping(script, store, expectedDirectoryName);
+    }
+
+    private TimeSeriesMapper prepareMapper(String script, ReadOnlyTimeSeriesStore store) {
         // Load mapping script
         TimeSeriesDslLoader dsl = new TimeSeriesDslLoader(script);
         TimeSeriesMappingConfig mappingConfig = dsl.load(network, mappingParameters, store, new DataTableStore(), null);
 
-        // Create NetworkPointWriter
-        DataSource dataSource = DataSourceUtil.createDataSource(networkOutputDir.resolve(network.getId()), null);
-        NetworkPointWriter networkPointWriter = new NetworkPointWriter(network, dataSource);
-
         // Create mapper
         TimeSeriesMappingLogger logger = new TimeSeriesMappingLogger();
         TimeSeriesMapperParameters parameters = new TimeSeriesMapperParameters(new TreeSet<>(Collections.singleton(1)),
-                Range.closed(0, 1), true, false, false, mappingParameters.getToleranceThreshold());
-        TimeSeriesMapper mapper = new TimeSeriesMapper(mappingConfig, parameters, network, logger);
+            Range.closed(0, 1), true, false, false, mappingParameters.getToleranceThreshold());
+        return new TimeSeriesMapper(mappingConfig, parameters, network, logger);
+    }
+
+    private void assertMapping(String script, ReadOnlyTimeSeriesStore store, String expectedDirectoryName) throws Exception {
+        // Create Mapper
+        TimeSeriesMapper mapper = prepareMapper(script, store);
+
         // Launch mapper
-        mapper.mapToNetwork(store, List.of(networkPointWriter));
+        mapper.mapToNetwork(store, networkPointWriterList);
 
         for (int point = 0; point < index.getPointCount(); point++) {
             String fileName = NetworkPointWriter.getFileName(network, 1, point, index) + ".xiidm";
