97b7b61a588194a7020e5cc7ddf4583e1c24a985
Nicolas Rol <nicolas.rol@rte-france.com>
2025-09-04 15:01:15 +0200

    fix TimeSeriesMappingConfigChecker
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- metrix-mapping/src/main/java/com/powsybl/metrix/mapping/TimeSeriesMappingConfigChecker.java
+++ metrix-mapping/src/main/java/com/powsybl/metrix/mapping/TimeSeriesMappingConfigChecker.java
@@ -9,7 +9,12 @@ package com.powsybl.metrix.mapping;
 
 import com.powsybl.metrix.mapping.exception.TimeSeriesMappingException;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.powsybl.metrix.mapping.EquipmentVariable.isVariableCompatible;
