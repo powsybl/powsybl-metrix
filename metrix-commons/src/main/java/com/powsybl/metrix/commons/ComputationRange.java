45e83a3a90965266bfd52ffc73e6ced366d81375
marifunf <marianne.funfrock@rte-france.com>
2025-12-01 11:18:06 +0100

    [IMG-2816] Missing import
    Signed-off-by: marifunf <marianne.funfrock@rte-france.com>

--- metrix-commons/src/main/java/com/powsybl/metrix/commons/ComputationRange.java
+++ metrix-commons/src/main/java/com/powsybl/metrix/commons/ComputationRange.java
@@ -13,6 +13,7 @@ import com.powsybl.timeseries.TimeSeriesFilter;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
